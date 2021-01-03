15e1a2cb070c743d0d2233ae1fa841ebb4ccce3e
rockship <rockship@rockships-MacBook-Pro.local>
2021-01-03 21:13:44 +0700

    [ work ] add nltk and reformat

---
+++ nltk/test_nltk.py
@@ -0,0 +1,29 @@
+import nltk
+from nltk.parse.corenlp import CoreNLPParser
+
+rules = []
+result = []
+
+file = open('./grammar.txt')
+lines = file.readlines()
+rules = ''.join(lines)
+
+grammar = nltk.CFG.fromstring(rules)
+
+inputs = open('./Data/test.txt')
+input_lines = inputs.readlines()
+
+parser = nltk.EarleyChartParser(grammar)
+f = open("output_nltk.txt", "w")
+
+for line in input_lines:
+    INDEX = 0
+    for tree in parser.parse(line.split() + '\n'):
+        if INDEX == 1:
+            continue
+        result.append(str(tree.pformat()))
+        f.writelines(str(tree.pformat()))
+        INDEX += 1
+
+f.close()
+print(result)
